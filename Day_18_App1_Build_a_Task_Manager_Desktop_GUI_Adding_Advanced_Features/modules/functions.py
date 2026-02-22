395e3492c3dde9e38967ab798d3e967dfeec8b42
mohdamirmir <aamir.mir111@gmail.com>
2026-02-22 22:39:41 +0400

    todo list web app

--- Day_18_App1_Build_a_Task_Manager_Desktop_GUI_Adding_Advanced_Features/modules/functions.py
+++ Day_18_App1_Build_a_Task_Manager_Desktop_GUI_Adding_Advanced_Features/modules/functions.py
@@ -1,11 +1,17 @@
 
 
 def get_todos(filepath="data/todos.txt"):
+    '''
+    Read a text file and return a list of to-do items
+    '''
     with open(filepath, "r") as local_file:
             todos_local = local_file.readlines()
     return todos_local
 
 def write_todos(todos_arg, filepath="data/todos.txt"):
+    '''
+    Write the to-d0 list items to the text file
+    '''    
     with open(filepath, "w") as local_file:
             local_file.writelines(todos_arg)
 
---
+++ Day_19_Build_a_Task_Manager/web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My Todo app")
+st.subheader("This is my todo app")
+st.text("This app is to increase your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Enter a todo:", 
+              placeholder="Add new todo",
+              on_change=add_todo,
+              key="new_todo")
+
+st.session_state
