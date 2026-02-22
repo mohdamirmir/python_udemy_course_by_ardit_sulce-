5b8db46ce6e61ff1ee75637cc97bf5001a906886
mohdamirmir <aamir.mir111@gmail.com>
2026-02-22 22:57:30 +0400

    app completion

--- Day_19_Build_a_Task_Manager/web.py
+++ Day_19_Build_a_Task_Manager/web.py
@@ -15,8 +15,13 @@ st.title("My Todo app")
 st.subheader("This is my todo app")
 st.text("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a todo:", 
               placeholder="Add new todo",
