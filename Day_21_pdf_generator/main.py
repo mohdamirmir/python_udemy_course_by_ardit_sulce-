97f8452b91ef67e76dd7debc7d8907aeebb0e0be
mohdamirmir <aamir.mir111@gmail.com>
2026-03-08 23:07:23 +0400

    adding pdf

--- Day_21_pdf_generator/main.py
+++ Day_21_pdf_generator/main.py
@@ -2,8 +2,20 @@ from fpdf import FPDF
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
 
-print(type(pdf))
+pdf.add_page()
+
+pdf.set_font(family="Times", style="B", size=12)
+
+pdf.cell(w=0, h=12, txt="Hello World", align="C", ln=1, border=1)
+pdf.cell(w=0, h=12, txt="Hi  There", align="C", ln=1, border=1)
+
 
 pdf.add_page()
 
-pdf.output("output.pdf")
+pdf.cell(w=0, h=12, txt="Hello World", align="C", ln=1, border=1)
+pdf.cell(w=0, h=12, txt="Hi  There", align="C", ln=1, border=1)
+
+
+
+
+pdf.output("./Day_21_pdf_generator/output.pdf")
